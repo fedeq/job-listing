f14579cc2c11e460de66442e232cfc8199f6c722
fedeq <federicoquattrocchio@gmail.com>
2024-01-04 14:40:06 -0300

    Use mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 export function Header() {
   return (
-    <header className="h-36 bg-header_desktop bg-cover bg-center bg-no-repeat"></header>
+    <header className="h-36 bg-header_mobile md:bg-header_desktop bg-cover bg-center bg-no-repeat"></header>
   );
 }
